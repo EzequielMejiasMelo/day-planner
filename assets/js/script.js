3848c1b952866d9aa822fff6d60803147beda04f
EzequielMejiasMelo <ezequiel.mejiasmelo@gmail.com>
2022-01-06 21:37:06 -0500

    Updated script.js
    Added comments, implement moment.js, adjusted row classes, added event listeners.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Object for localstorage and initial rendering
 var tasks = {
   "9am": "",
   "10am": "",
@@ -10,37 +11,49 @@ var tasks = {
   "5pm": "",
 };
 
+//keys array with list of times
 var keys = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
+//Querying DOM elements
 var bodyContainer = $(".container");
+var todaySpan = $("#currentDay");
 
+//Gets current day in "Sunday, September 3rd" format
+var day = moment().format("dddd, MMMM Do");
+
+//checks if localstorage contains object and then renders page
 function pullTimes() {
+  //sets time
+  todaySpan.text(day);
+
+  //checks if obj in local store
   temp = JSON.parse(localStorage.getItem("tasks"));
   if (temp) {
     tasks = temp;
   }
+  //calls funct to render page
   renderTimes();
+  return;
 }
 
+//renders DOM elements to page and pulls from updated tasks object
 function renderTimes() {
+  //loops through keys and renders row for each
   for (var i = 0; i < keys.length; i++) {
     let keyIndex = keys[i];
 
     //Render DOM elements
-    var taskRow = $("<div>").addClass("row");
+    var taskRow = $("<div>").addClass("row").attr("data-index", keyIndex);
     var pTag = $("<p>")
-      .addClass("col-2 text-right p-2")
-      .attr("id", "hour")
+      .addClass("col-2 text-right p-2 hour")
       .text(keyIndex);
     var textAreaTag = $("<textarea>")
-      .addClass("col-8")
+      .addClass("col-8 ")
       .attr("id", "taskItem")
       .text(tasks[keyIndex]);
     var iconTag = $("<i>").addClass("fas fa-save");
     var buttonTag = $("<button>")
-      .addClass("col-2 btn")
-      .attr("id", "saveBtn")
-      .attr("data-index", keyIndex)
+      .addClass("col-2 btn saveBtn")
       .append(iconTag);
 
     //append DOM elements to page
@@ -52,8 +65,12 @@ function renderTimes() {
   return;
 }
 
-function setTimes() {
+function setTimes(event) {
+
   localStorage.setItem("tasks", JSON.stringify(tasks));
+  return;
 }
 
-pullTimes();
+$(".saveBtn").on("click", setTimes);
+
+$(document).ready(pullTimes);
