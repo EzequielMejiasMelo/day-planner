ed3d1c376fc8fb4b0cebe2c52c271007f51c2c9a
EzequielMejiasMelo <ezequiel.mejiasmelo@gmail.com>
2022-01-06 22:05:59 -0500

    Update script.js
    Made changes to render classes based on current time.

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ var tasks = {
 };
 
 //keys array with list of times
-var keys = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+var keys = [[9, "9am"], [10, "10am"], [11, "11am"], [12, "12pm"], [13, "1pm"], [14, "2pm"], [15, "3pm"], [16, "4pm"], [17, "5pm"]];
 
 //Querying DOM elements
 var bodyContainer = $(".container");
@@ -20,6 +20,7 @@ var todaySpan = $("#currentDay");
 
 //Gets current day in "Sunday, September 3rd" format
 var day = moment().format("dddd, MMMM Do");
+var currentHour = moment().hour();
 
 //checks if localstorage contains object and then renders page
 function pullTimes() {
@@ -40,21 +41,31 @@ function pullTimes() {
 function renderTimes() {
   //loops through keys and renders row for each
   for (var i = 0; i < keys.length; i++) {
-    let keyIndex = keys[i];
+    let keyHour = keys[i][0];
+    let keyTime = keys[i][1];
 
     //Render DOM elements
-    var taskRow = $("<div>").addClass("row").attr("data-index", keyIndex);
+    var taskRow = $("<div>").addClass("row").attr("data-index", keyHour);
     var pTag = $("<p>")
       .addClass("col-2 text-right p-2 hour")
-      .text(keyIndex);
+      .text(keyTime);
     var textAreaTag = $("<textarea>")
       .addClass("col-8 ")
       .attr("id", "taskItem")
-      .text(tasks[keyIndex]);
+      .text(tasks[keyTime]);
     var iconTag = $("<i>").addClass("fas fa-save");
     var buttonTag = $("<button>")
       .addClass("col-2 btn saveBtn")
       .append(iconTag);
+
+    //sets time based classes
+    if (currentHour > 17 || currentHour > keyHour){
+      textAreaTag.addClass("past");
+    } else if (currentHour < 9 || currentHour < keyHour){
+      textAreaTag.addClass("future");
+    } else {
+      textAreaTag.addClass("present");
+    }
 
     //append DOM elements to page
     taskRow.append(pTag);
@@ -66,11 +77,8 @@ function renderTimes() {
 }
 
 function setTimes(event) {
-
   localStorage.setItem("tasks", JSON.stringify(tasks));
   return;
 }
-
-$(".saveBtn").on("click", setTimes);
 
 $(document).ready(pullTimes);
